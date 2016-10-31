405c0f8dd6d2d433f89462f5370b4b2d1ef5835a
Michiko Inaba <michiko.luther@gmail.com>
2016-10-31 12:33:17 -0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,14 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'live4ever'
+    },
+	'database': 'tool_rental',
+    'users_table': 'users',
+    'payments_table':'payments',
+    'customers_table':'customers',
+    'rented_tools_table':'rented_tools',
+    'tools_table':'tools'
+};
